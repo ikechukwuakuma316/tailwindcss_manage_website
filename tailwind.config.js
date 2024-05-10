a8014db0783b42fb3ee9205f9a39dc8a42dd4030
Ikechukwu Akuma <savvy@Ikechukwus-MacBook-Pro.local>
2024-05-09 21:52:22 -0400

    new commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ['./build/*.html', './build/js/*.js'],
   theme: {
     screens: {
       sm: '480px',
